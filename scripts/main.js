ea10edf0440134ab7acbc32584d52f7d9da66875
Daniil Kovalenko <dizelgerda@yandex.ru>
2021-06-15 23:28:34 +0300

    hotfix

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,19 @@ const buttonOpenPopupAddCard = document.querySelector('.account__add-button');
 
 function openPopup(popup) {
     popup.classList.add('popup_opened');
+    popup.addEventListener('click', closeByClick);
     document.addEventListener('keydown', closeByEscape);
 }
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
+    popup.removeEventListener('click', closeByClick);
     document.removeEventListener('keydown', closeByEscape);
 }
 
 function closeByEscape(evt) { if (evt.key == 'Escape') closePopup(document.querySelector('.popup_opened')); }
 
-
-Array.from(document.querySelectorAll('.popup')).forEach((popup) => {
-    popup.addEventListener('click', (evt) => {
-        if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
-            closePopup(evt.currentTarget);
-        }
-    })
-});
+function closeByClick(evt) {
+    if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+        closePopup(evt.currentTarget);
+    }
+}
